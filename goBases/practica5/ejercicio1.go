e255f539b9b2e184822073f25ca371cfa9e0ce3e
luiscarvill <luis.carvajal@mercadolibre.com.co>
2022-09-27 13:32:05 -0500

    Ejercicios practica C3 - Go Bases TM

--- goBases/practica5/ejercicio1.go
+++ goBases/practica5/ejercicio1.go
@@ -4,6 +4,15 @@ import (
 	"os"
 )
 
+/*
+Ejercicio 1 - Guardar archivo
+Una empresa que se encarga de vender productos de limpieza necesita:
+Implementar una funcionalidad para guardar un archivo de texto, con la información de productos comprados, separados por punto y coma (csv).
+Debe tener el id del producto, precio y la cantidad.
+Estos valores pueden ser hardcodeados o escritos en duro en una variable.
+
+*/
+
 type Producto interface {
 	ConcatProduct() string
 }
